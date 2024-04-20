fda48dfe6da54b785ce1c0b6eda10d8f532135af
Rajithxx <148357145+Rajithxx@users.noreply.github.com>
2024-04-20 12:56:26 +0530

    212221060222 FULL_SUBTRACTOR.v
    Rajith Rufus

--- FULL_SUBTRACTOR.srcs/sources_1/new/FULL_SUBTRACTOR.v
+++ FULL_SUBTRACTOR.srcs/sources_1/new/FULL_SUBTRACTOR.v
@@ -1,28 +1,6 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.01.2024 14:44:43
-// Design Name: 
-// Module Name: FULL_SUBTRACTOR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module full_subtractor(a, b, c,D, Bout);
-input a, b, c;
-output D, Bout;
-                               //Write the logic for full adder in Dataflow modeling
-  
-endmodule  
+module fs(a,b,c,diff,borrow);
+input a,b,c;
+output diff,borrow;
+assign diff=a^b^c;
+assign borrow=((~a)&c)|((~a)&b)|(b&c);
+endmodule
